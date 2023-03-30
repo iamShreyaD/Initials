2b71c2bca889d5e357da514ef7d02c94edc3323f
Shreya B Deshpande <shreyavbdeshpande@gmail.com>
2023-03-30 14:51:32 -0400

    This project is done through Codecademy

---
+++ Initials.py
@@ -0,0 +1,22 @@
+
+#Shreya B Deshpande
+#This project is done through Codecademy
+#This project prints the presented initials of the first name and last name 
+first_name = '''
+ SSS
+S   S
+S  
+ SSS
+    S
+S   S
+ SSS'''
+last_name = '''
+DDDD
+D   D
+D   D
+D   D
+D   D
+D   D
+DDDD'''
+name = first_name + last_name
+print(name)
